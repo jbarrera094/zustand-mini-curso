ad6e238020dc61a699498cba0bebbabb0f03f78a
jbarrera094 <jbarrera094@gmail.com>
2024-10-11 08:03:25 -0500

    feat(increaseBears): read and increase all bears

---
+++ src/stores/bears/bears.store.ts
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+
+interface BearStore {
+  blackBears: number;
+  polarBears: number;
+  pandaBears: number;
+
+  increaseBlackBears: (by: number) => void;
+  increasePolarBears: (by: number) => void;
+  increasePandaBears: (by: number) => void;
+}
+
+export const useBearStore = create<BearStore>((set) => ({
+  blackBears: 10,
+  polarBears: 5,
+  pandaBears: 15,
+
+  increaseBlackBears: (by: number) =>
+    set((state) => ({ blackBears: state.blackBears + by })),
+  increasePolarBears: (by: number) =>
+    set((state) => ({ polarBears: state.polarBears + by })),
+  increasePandaBears: (by: number) =>
+    set((state) => ({ pandaBears: state.pandaBears + by })),
+}));
